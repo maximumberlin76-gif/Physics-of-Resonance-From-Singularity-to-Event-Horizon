6196c743f1a5faa5cc838fa601d12f211c74e61b
Maksym Marnov/Марнов Максим <maximumberlin76@gmail.com>
2026-07-08 20:42:30 +0200

    Enhance RPU core public API with new functions
    Added public API components to the RPU core module.

--- rpu_core/__init__.py
+++ rpu_core/__init__.py
@@ -1,1 +1,15 @@
-from .rpu_core_v1 import RPUCore
+"""Public API for the RPU resonant core."""
+
+from .rpu_core_v1 import (
+    RPUCore,
+    power_iteration_radius,
+    quantize_balanced,
+    quantize_W,
+)
+
+__all__ = (
+    "RPUCore",
+    "power_iteration_radius",
+    "quantize_balanced",
+    "quantize_W",
+)
